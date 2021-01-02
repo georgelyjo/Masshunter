4c1dfdbda0a10b4a73bba5221d8e24e6de1ef307
george <georgelyjo@gmail.com>
2021-01-01 23:54:09 -0500

    new commit

--- subdomain.sh
+++ subdomain.sh
@@ -11,5 +11,7 @@ amass enum -d $1 -o $dir/$1_subdomains;
 cat /root/bug/$1/sub.txt | anew $dir/$1_subdomains;
 echo "crt.sh"
 curl -s https://crt.sh/\?q\=\%.$1\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | anew $dir/$1_subdomains;
+echo "subdrill"
+cd /root/arsenal/Sub-Drill/ && bash subdrill.sh $1 | anew $dir/$1_subdomains;
 
 
